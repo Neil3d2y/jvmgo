6fb18a633d8c54723b854c6aac32d27bda2ee32e
neil3d2y <zjcneil029@gmail.com>
2024-09-09 21:53:59 -0700

    JVM entry point

---
+++ ch01/cmd.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag" // used for parsing command-line flags (args)
+	"fmt"  // provide formatted I/O functions
+	"os"   // OS functionality
+)
+
+type Cmd struct {
+	helpFlag    bool
+	versionFlag bool
+	cpOption    string
+	class       string
+	args        []string
+}
+
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+	return cmd
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-option] class [args...]\n", os.Args[0])
+}
---
+++ ch01/go.mod
@@ -0,0 +1,3 @@
+module neil3d2y.com/jvmgo/ch01
+
+go 1.20
